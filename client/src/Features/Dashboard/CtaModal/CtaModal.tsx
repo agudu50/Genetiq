bee8bc5b85b30eb358198cd2a0ed7bcd5fac5033
Anthony Gudu <anthonygudu2020@gmail.com>
2026-06-06 17:17:01 +0000

    optimised all pages to load faster

--- client/src/Features/Dashboard/CtaModal/CtaModal.tsx
+++ client/src/Features/Dashboard/CtaModal/CtaModal.tsx
@@ -1,14 +1,16 @@
 import ReactDOM from "react-dom";
 import styles from "./CtaModal.module.scss";
 import Cross from "@assets/CtaModal/Cross.svg?react";
-import Cart from "@assets/CtaModal/Cart.svg?react";
 import Shape from "@assets/CtaModal/Shape.svg?react";
 import ChevronHollow from "@assets/CtaModal/ChevronHollow.svg?react";
 import { useState } from "react";
 import { useLanguage } from "@/App/i18n/LanguageContext";
+import { useNavigate } from "react-router-dom";
+import { paths } from "@/App/Routes/Paths";
 
 const CtaModal = () => {
 	const { t } = useLanguage();
+	const navigate = useNavigate();
 	const [isClosed, setIsClosed] = useState(false);
 
 	return ReactDOM.createPortal(
@@ -36,13 +38,13 @@ const CtaModal = () => {
 			<div className={styles["CtaModal-body"]}>
 				<p className={styles["CtaModal-description"]}>{t("cta_description")}</p>
 			</div>
-			<button className={styles["CtaModal-button"]}>
+			<button 
+				className={styles["CtaModal-button"]}
+				onClick={() => navigate(paths.config.tests)}
+			>
 				<p className={styles["CtaModal-button-text"]}>
 					{t("cta_order_dna_test")}
 				</p>
-				<div className={styles["CtaModal-cart-container"]}>
-					<Cart />
-				</div>
 			</button>
 
 			<div
