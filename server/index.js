32d13ed90068c5a84f9222c354f9303c2d2fa400
Anthony Gudu <anthonygudu2020@gmail.com>
2026-07-09 16:34:00 +0000

    fix: increase json payload limit to 200mb

--- server/index.js
+++ server/index.js
@@ -12,7 +12,8 @@ const socket = require("socket.io");
 const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:3000";
 
 app.use(cors({ origin: CLIENT_URL, credentials: true }));
-app.use(express.json());
+app.use(express.json({ limit: "200mb" }));
+app.use(express.urlencoded({ limit: "200mb", extended: true }));
 
 
 
